6cd54792086c4746fde67e52a05b692be78253be
yaiba2307 <trungbuias@gmail.com>
2026-06-02 09:33:40 +0200

    chore(migrations): merge photos and company-role heads into single chain
    Two migrations (add_project_photos c78cbcf5a73b, add_company_role c9a1b7e3d2f5)
    branched from the same parent in back-to-back PRs. Empty merge migration to
    make the Alembic chain linear. No schema changes.

---
+++ migrations/versions/0e9904e84af6_merge_photos_and_company_role_heads.py
@@ -0,0 +1,21 @@
+"""merge photos and company role heads
+
+Revision ID: 0e9904e84af6
+Revises: c78cbcf5a73b, c9a1b7e3d2f5
+Create Date: 2026-06-02 09:30:00.000000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "0e9904e84af6"
+down_revision = ("c78cbcf5a73b", "c9a1b7e3d2f5")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
