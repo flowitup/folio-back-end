86155961e0011f80e5450c012e2478a31cdddb90
yaiba2307 <trungbuias@gmail.com>
2026-06-02 09:27:03 +0200

    chore(migrations): merge photos and company-role heads into single chain

---
+++ migrations/versions/a1b2c3d4e5f6_merge_photos_and_company_role_heads.py
@@ -0,0 +1,21 @@
+"""merge photos and company role heads
+
+Revision ID: a1b2c3d4e5f6
+Revises: c78cbcf5a73b, c9a1b7e3d2f5
+Create Date: 2026-06-02 09:20:00.000000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "a1b2c3d4e5f6"
+down_revision = ("c78cbcf5a73b", "c9a1b7e3d2f5")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
